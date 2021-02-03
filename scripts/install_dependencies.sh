0959fb37438c41ef7802333fc195a07b7bea1e4b
luisbarrague <luis.barrague@gmail.com>
2021-02-03 13:30:52 -0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+install_dir="/home/ec2-user/GoSample"
+
 sudo yum install golang -y
+
+### Check for dir, if not found create it using the mkdir ##
+[ ! -d "$dldir" ] && mkdir -p "$install_dir"

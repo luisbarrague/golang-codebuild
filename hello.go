038d3bdf2aef432981a1075daa5d362875feeec1
Luis Barragué <75748410+luis-barrague-sw@users.noreply.github.com>
2021-02-04 09:04:44 -0300

    ADD: Modify listen address

--- hello.go
+++ hello.go
@@ -14,7 +14,7 @@ func homePage(w http.ResponseWriter, r *http.Request){
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":80", nil))
+	log.Fatal(http.ListenAndServe("0.0.0.0:80", nil))
 }
 
 func main() {
